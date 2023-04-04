8ce05024c488c840efa6af14d16262e1f7637f22
Artur Medvedev <varouser@gmail.com>
2023-04-04 08:46:55 +0600

    Fix whitespace

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -18,7 +18,7 @@ def ask_question_prime():
 def get_correct_answer(num):
     if num <= 1:
         return 'no'
-    for i in range(2, int(num**0.5)+1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return 'no'
     return 'yes'
