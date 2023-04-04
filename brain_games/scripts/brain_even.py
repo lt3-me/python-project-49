cc96e1e1ebe6a4f4ff20ec7d86390b2a2b10e19f
Artur Medvedev <varouser@gmail.com>
2023-04-04 08:22:33 +0600

    Revert "Move game logic into separate files"
    This reverts commit 43c70e880ac2dc618898cd7994f9f9a27fbd6c13.

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
-from brain_games.games.game import start_game
-from brain_games.games.even_game import DESC, ask_question_even
+from random import randint
+import prompt
+from brain_games.game import start_game
+
+MIN_RAND_NUM_VALUE = 1
+MAX_RAND_NUM_VALUE = 99
+DESC = 'Answer "yes" if the number is even, otherwise answer "no".'
+
 
 def main():
     start_game(DESC, ask_question_even)
+
+
+def ask_question_even():
+    random_number = randint(MIN_RAND_NUM_VALUE, MAX_RAND_NUM_VALUE)
+    print(f'Question: {random_number}')
+    answer = prompt.string('Your answer: ')
+    correct_answer = get_correct_answer(random_number)
+    return (answer.lower(), correct_answer)
+
+
+def get_correct_answer(num):
+    is_even = num % 2 == 0
+    if is_even:
+        return 'yes'
+    else:
+        return 'no'
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
-from brain_games.games.game import start_game
-from brain_games.games.prime_game import DESC, ask_question_prime
+from random import randint
+import prompt
+from brain_games.game import start_game
+
+MIN_RAND_NUM_VALUE = 0
+MAX_RAND_NUM_VALUE = 230
+DESC = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def main():
     start_game(DESC, ask_question_prime)
+
+
+def ask_question_prime():
+    random_number = randint(MIN_RAND_NUM_VALUE, MAX_RAND_NUM_VALUE)
+    print(f'Question: {random_number}')
+    answer = prompt.string('Your answer: ')
+    correct_answer = get_correct_answer(random_number)
+    return (answer.lower(), correct_answer)
+
+
+def get_correct_answer(num):
+    if num <= 1:
+        return 'no'
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return 'no'
+    return 'yes'
 
 
 if __name__ == '__main__':
