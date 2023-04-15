d298be113b1728341c9bae984c6d9699d08314a1
Artur Medvedev <varouser@gmail.com>
2023-04-15 20:52:30 +0600

    Refactor generate function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,15 +4,11 @@ DESC = 'Find the greatest common divisor of given numbers.'
 
 
 def generate_question():
-    correct_gcd = randint(2, 25)
-    random_multipliers = [randint(2,
-                                  25 // correct_gcd)
-                          for _ in range(2)]
-    mult_gcd = gcd_list(random_multipliers)
-    random_multipliers = [number // mult_gcd for number in random_multipliers]
-    random_numbers = [number * correct_gcd for number in random_multipliers]
+    first_number = randint(1, 100)
+    second_number = randint(1, 100)
 
-    question = " ".join(str(num) for num in random_numbers)
+    correct_gcd = gcd(first_number, second_number)
+    question = f"{first_number} {second_number}"
     correct_gcd = str(correct_gcd)
 
     return (question, correct_gcd)
@@ -22,10 +18,3 @@ def gcd(num1, num2):
     while num2:
         num1, num2 = num2, num1 % num2
     return num1
-
-
-def gcd_list(numbers):
-    result = numbers[0]
-    for i in range(1, len(numbers)):
-        result = gcd(result, numbers[i])
-    return result
