5a245d3ec71626d254c176a66ecf54dc80cb6dbc
Artur Medvedev <varouser@gmail.com>
2023-04-15 21:00:34 +0600

    Refactor generation function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,31 +1,23 @@
 from random import randint
 
-MAX_RAND_START_VALUE = 100
-MIN_RAND_COMMON_DIFF = 2
-MAX_RAND_COMMON_DIFF = 20
 NUMBER_OF_ELEMENTS = 10
 
 DESC = 'What number is missing in the progression?'
 
 
 def generate_question():
-    common_difference = randint(MIN_RAND_COMMON_DIFF,
-                                MAX_RAND_COMMON_DIFF)
-    start_value = randint(0, MAX_RAND_START_VALUE)
+    common_difference = randint(2, 20)
+    start_value = randint(0, 100)
     stop_value = start_value + \
         common_difference * NUMBER_OF_ELEMENTS
     progression = list(range(start_value, stop_value,
                              common_difference))
     missing_element = randint(0, 9)
-    progression_string = ''
-    for id, elem in enumerate(progression):
-        if id != missing_element:
-            progression_string = progression_string + \
-                str(elem) + ' '
-        else:
-            progression_string = progression_string + '.. '
 
-    progression_string = progression_string.strip()
     correct_answer = str(progression[missing_element])
+    progression[missing_element] = '..'
+
+    progression = [str(x) for x in progression]
+    progression_string = ' '.join(progression)
 
     return (progression_string, correct_answer)
